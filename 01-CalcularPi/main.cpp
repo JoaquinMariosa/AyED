9b01917bffa359a0317506a624d63981c81421c3
Joaquín Mariosa <jmariosa@frba.utn.edu.ar>
2023-06-24 19:33:21 -0300

    Update main.cpp

--- 01-CalcularPi/main.cpp
+++ 01-CalcularPi/main.cpp
@@ -5,14 +5,20 @@ using namespace std;
 
 int main() {
     double pi = 0;
+    double pifinal = 0;
+    double n = 0;
+    int i = 0;
+    double dif = 0;
 
-    for (int i = 0; i <= 4000000; i++) {
-        double n = (pow(-1, i))/(2*i+1);
+    do {
+        n = (pow(-1, i))/(2*i+1);
         pi += n;
-    }
+        pifinal = pi * 4;
+        dif = pifinal - 3.141592;
+        i++;
+    } while ((abs(dif)) > 0.0000001);
 
-    pi *= 4;
-    cout << setprecision(7) << fixed << "El valor de pi es de: " << pi << endl;
+    cout << setprecision(6) << fixed << "El valor de pi es de: " << pifinal << endl;
 
     return 0;
 }
